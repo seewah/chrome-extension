ce0a0c81caeab948ccd3275ce24e1751f2eff5ee
See Wah Cheng <seewah.cheng@mendeley.com>
2011-06-08 04:43:35 +0100

    doing a proper null check instead

--- scripts/feed.js
+++ scripts/feed.js
@@ -79,7 +79,7 @@ function isItemUnread(itemUri, currentFeedUriMap, viewedFeedUriMap) {
  */
 function createUriMap(feedJson) {
 	var map = {};
-	if(feedJson) {
+	if(feedJson !== null) {
 		feedItems = JSON.parse(feedJson);
 		if(feedItems !== null) {
 			for(var i = 0; i < feedItems.length; i++) {
